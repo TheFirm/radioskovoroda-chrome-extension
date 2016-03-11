ed7afb3c4b9ccdf8ee4707e61b39fe3c02eb9a4e
uhtyshka <uhtshka@gmail.com>
2016-03-11 15:40:02 +0200

    added new icons

--- background.js
+++ background.js
@@ -1,6 +1,11 @@
 var isOn   = false;
 
 chrome.runtime.onMessage.addListener(function(msg, sender){
+    
+    chrome.browserAction.setIcon({
+        path: isOn ? './play-brws.png': './pause-brws.png' 
+    });
+
     var stream = document.getElementById('radio-skovoroda');
     isOn ? stream.pause() : stream.play();
     isOn = !isOn;
