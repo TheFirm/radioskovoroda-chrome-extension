c2bfb1d14d9885ee1d4b4c3f5d9446cfd2aecc57
uhtyshka <uhtshka@gmail.com>
2016-03-15 18:29:32 +0200

    added track titles, refactored

--- background.js
+++ background.js
@@ -1,12 +1,46 @@
-var isOn = false;
+var isOn     = false,
+    callData = {
+    type  : 'GET',
+    url   : 'http://195.248.234.62:8000/json.xsl',
+    async : true
+};
 
-chrome.runtime.onMessage.addListener(function(msg, sender){
-    
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+    switch (request.msg){
+        case 'trigger-radio':
+            triggerRadio();
+            break;
+        case 'get-track-data':
+            fetchTrackData(callData, sendResponse);
+            break;
+    }
+});
+
+function triggerRadio(){
+    var stream = document.getElementById('radio-skovoroda');
+
     chrome.browserAction.setIcon({
         path: isOn ? './play-brws.png': './pause-brws.png' 
     });
-
-    var stream = document.getElementById('radio-skovoroda');
+    
+    // need to refactor
     isOn ? stream.pause() : stream.play();
     isOn = !isOn;
-});
+}
+
+function fetchTrackData(callData, callback){
+    return $.ajax(callData).done(function(data, status){
+        if(status !== "success") return badFetch();
+        chrome.extension.sendMessage({msg: 'current-track', track: parseResp(data)["title"]});
+    });
+}
+
+function badFetch(){
+    chrome.extension.sendMessage({msg: 'current-track', track: ""});
+}
+
+function parseResp(str){
+    return JSON.parse(str.split('parseMusic(')[1].split(');')[0])['/radioskovoroda'];
+}
+
+
--- content.js
+++ content.js
@@ -1,19 +1,56 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-    var btn        = document.getElementById('btn'),
-        onAirClass = 'on-air';
+    var btn         = document.getElementById('btn'),
+        onAirClass  = 'on-air',
+        loopInterval = 15000;
 
-    if(isOn()) btn.classList.add(onAirClass);
+    if(isOn()) {
+        btn.classList.add(onAirClass); 
+    }
 
     btn.addEventListener('click', clickHandler);
+    startTrackDataLoop(loopInterval);
+
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+        switch (request.msg){
+            case 'current-track':
+                updateTrack(request.track);
+                break;
+        }
+    });
 
     function clickHandler(){
-        isOn() ? btn.classList.remove(onAirClass) : btn.classList.add(onAirClass);
-        return chrome.extension.sendMessage('btn-clicked');
+        if(isOn()){
+            btn.classList.remove(onAirClass)
+        } else {
+            btn.classList.add(onAirClass)
+        }
+        chrome.extension.sendMessage({msg: 'trigger-radio'});
+    }
+
+    function startTrackDataLoop(interval){
+        fetchTrackData();
+        setInterval(function(){
+            fetchTrackData();
+        }, interval);
+    }
+
+    function fetchTrackData(){
+        chrome.extension.sendMessage({msg: 'get-track-data'});
     }
 
     function isOn(){
         return chrome.extension.getBackgroundPage().isOn;
     }
 
+    function updateTrack(track){
+        track = track || '';
+        if(!track) return hideTrackHolder();
+        $('div.track').show().find('#track-title').html(track);
+    }
+
+    function hideTrackHolder(){
+        return $('div.track').hide();
+    }
 });
+
