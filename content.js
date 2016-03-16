f6b71ef05dac1c71dd6c3c7b5c9c77000bf81aaf
uhtyshka <uhtshka@gmail.com>
2016-03-16 17:32:27 +0200

    added soc-search

--- content.js
+++ content.js
@@ -54,12 +54,61 @@ document.addEventListener('DOMContentLoaded', function(){
             return hideTrackHolder();
         } else {
             $('div.track').show().find('#track-title').html(track);
+            updateSocSearch(track);
         }
     }
 
 
+    function updateSocSearch(track){
+        var socLinks      = $('div.soc-search'),
+            searchQueries = buildQueries(track);
+        for(var service in searchQueries){
+           socLinks.find('a.socicon-' + service).attr('href', searchQueries[service]);
+        }
+    }
+
+
+    function buildQueries(track){
+        var protocol = "http://",
+            queries  = {
+                vkontakte  : "",
+                youtube    : "",
+                soundcloud : "",
+                deezer     : ""
+            };
+
+        for (var service in queries) {
+            var q = "";
+            switch(service){
+                case "vkontakte" :
+                    q = '/search?c%5Bq%5D=' + track + '&c%5Bsection%5D=audio';
+                    queries.vkontakte = createQuery(protocol, 'vk.com', q);
+                    break;
+                case "youtube"   :
+                    q = '/results?search_query=' + track;
+                    queries.youtube = createQuery(protocol, 'youtube.com', q);
+                    break;
+                case "soundcloud":
+                    q = '/search?q=' + track;
+                    queries.soundcloud = createQuery(protocol, 'soundcloud.com',q);
+                    break;
+                case "deezer"    :
+                    q = '/search/' + track;
+                    queries.deezer = createQuery(protocol, 'deezer.com',q);
+                    break;
+            }
+        }
+
+        function createQuery(protocol, domen, query){
+            return protocol + domen + query;
+        }
+
+        return queries;
+    }
+
+
     function hideTrackHolder(){
-        return $('div.track').hide();
+        return $('div.track-items').hide();
     }
 
 });
