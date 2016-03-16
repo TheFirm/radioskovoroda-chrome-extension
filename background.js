643d832af014a898c068d971c80b62cfe69ba6ab
uhtyshka <uhtshka@gmail.com>
2016-03-16 14:55:18 +0200

    refactored background

--- background.js
+++ background.js
@@ -1,11 +1,14 @@
 var isOn     = false,
     callData = {
-    type  : 'GET',
-    url   : 'http://195.248.234.62:8000/json.xsl',
-    async : true
-};
+        type  : 'GET',
+        url   : 'http://195.248.234.62:8000/json.xsl',
+        async : true
+    };
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+chrome.runtime.onMessage.addListener(messageListener);
+
+
+function messageListener(request, sender, sendResponse){
     switch (request.msg){
         case 'trigger-radio':
             triggerRadio();
@@ -14,30 +17,62 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
             fetchTrackData(callData, sendResponse);
             break;
     }
-});
+}
+
 
 function triggerRadio(){
     var stream = document.getElementById('radio-skovoroda');
 
+    if(isOn){
+        enableRadio(stream);
+    } else {
+        disableRadio(stream);
+    }
+}
+
+
+function enableRadio(radio){
     chrome.browserAction.setIcon({
-        path: isOn ? './play-brws.png': './pause-brws.png' 
+        path: './play-brws.png'
     });
-    
-    // need to refactor
-    isOn ? stream.pause() : stream.play();
+
+    radio.play();
     isOn = !isOn;
 }
 
-function fetchTrackData(callData, callback){
-    return $.ajax(callData).done(function(data, status){
-        if(status !== "success") return badFetch();
-        chrome.extension.sendMessage({msg: 'current-track', track: parseResp(data)["title"]});
+
+function disableRadio(radio){
+    chrome.browserAction.setIcon({
+        path: './play-brws.png'
     });
+
+    radio.pause();
+    isOn = !isOn;
 }
+
+
+function fetchTrackData(callData, callback){
+
+    function trackDataCallback(data, status){
+        if(status !== "success") {
+            return badFetch();
+        } else{
+            var message = {
+                msg   : 'current-track', 
+                track : parseResp(data)["title"]
+            };
+            chrome.extension.sendMessage(message);
+        }
+    }
+
+    return $.ajax(callData).done(trackDataCallback);
+}
+
 
 function badFetch(){
     chrome.extension.sendMessage({msg: 'current-track', track: ""});
 }
+
 
 function parseResp(str){
     return JSON.parse(str.split('parseMusic(')[1].split(');')[0])['/radioskovoroda'];
